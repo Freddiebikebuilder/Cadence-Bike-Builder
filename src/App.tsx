e4d9bcc96b8b90bae57e40f621d11b1f5f400f4a
Freddiebikebuilder <fh230408@gmail.com>
2025-07-22 23:53:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Index from './pages/Index';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
